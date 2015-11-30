05d0d90a1f87812f72401ed0d73c1a0ba5131107
deskach <dzianis.kachanau@gmail.com>
2015-11-30 17:05:35 +1100

    Introduced build task in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,7 @@
           {
             expand: true,
             flatten: true,
-            src: ['bootstrap/*'], dest: '../Web/bootstrap/',
-            filter: 'isFile'
-          },
-          {
-            expand: true,
-            flatten: true,
-            src: ['css/*'], dest: '../Web/css/',
+            src: ['style/*'], dest: '../Web/style/',
             filter: 'isFile'
           },
           {
@@ -36,13 +30,7 @@
             flatten: true,
             src: ['js/*'], dest: '../Web/js/',
             filter: 'isFile'
-          },
-          {
-            expand: true,
-            flatten: true,
-            src: ['media/*'], dest: '../Web/media/',
-            filter: 'isFile'
-          },
+          }
         ],
       },
     },
