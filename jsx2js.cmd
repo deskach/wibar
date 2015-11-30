6df4d4f4cf7853144309f4f3783f077cbe78b553
deskach <dzianis.kachanau@gmail.com>
2015-11-30 17:15:07 +1100

    removed dev-files

--- jsx2js.cmd
+++
@@ -1,1 +0,0 @@
-C:\Users\%username%\AppData\Roaming\npm\jsx -x jsx "C:\Users\Dzianis\Documents\Visual Studio 2015\Projects\wibar\wibar\jsx" "C:\Users\Dzianis\Documents\Visual Studio 2015\Projects\wibar\wibar\js"
